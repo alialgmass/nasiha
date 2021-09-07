6c94a7e8d49d383dfe1f61a62d3fc8a67544b778
ali <eali7563@gmail.com>
2021-09-07 08:26:38 +0200

    Create a React app and publish it to GitHub Pages

---
+++ src/componet/Navbar/index.js
@@ -0,0 +1,24 @@
+import React from "react";
+import './navbar.css';
+const Navbar=()=>{
+    return(<div className="navbar">
+<div className="container">
+    <div className="logo">logo</div>
+    <ul>
+        <li>
+           home
+        </li>
+        <li>
+           home
+        </li>
+        <li>
+           home
+        </li>
+        <li>
+           home
+        </li>
+    </ul>
+</div>
+    </div>);
+}
+export default Navbar;
---
+++ src/componet/home/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import './home.css';
+const Home=()=>{
+    return(<div className="Home">
+        
+<div className="container">
+   
+</div>
+    </div>);
+}
+export default Home;
